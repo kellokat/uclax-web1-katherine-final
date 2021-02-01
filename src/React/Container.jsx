8879fc303a3e10e98402abdf25348ea3d8fee00c
Katherine Crowe <kellokat@gmail.com>
2021-02-01 15:01:01 -0500

    Scaffolding our website

---
+++ src/React/Container.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './Header.jsx';
+import Nav from './Nav.jsx';
+import Main from './Main.jsx';
+import Footer from './Footer.jsx';
+
+const Container = () => {
+
+    return (
+        <BrowserRouter>
+            <div className='Container'>
+                <Header />
+                <Nav />
+                <Main />
+                <Footer />
+             </div>
+        </BrowserRouter>
+    );
+}
+
+export default Container;
---
+++ src/React/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+/* Components ----------------------*/
+import Welcome from './Pages/Welcome/Welcome.jsx';
+import Services from './Pages/Services/Services.jsx';
+import Contact from './Pages/Contact/Contact.jsx';
+import Login from './Pages/Login/Login.jsx';
+
+const Main = () => {
+    return (
+        <main>
+                <Switch>
+                        <Route path='/services' component={ Services } />
+                        <Route path='/contact' component={ Contact } />
+                        <Route path='/login' component={ Login } />
+                        <Route path='/' component={ Welcome } exact/>
+                </Switch>
+        </main>
+    )
+}
+
+export default Main;
